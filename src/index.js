a7652f36c56eb95e2fb32abfb049d3a1805c029a
Bardan P <me@bardanp.com>
2024-05-09 23:44:19 -0400

    Updated Title

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-// Import necessary modules
 import React from "react";
-import { createRoot } from "react-dom/client"; // Import createRoot
+import { createRoot } from "react-dom/client"; 
 import App from "./App";
 
-// Create the root container using createRoot
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-// Render the App component
 root.render(<App />);
